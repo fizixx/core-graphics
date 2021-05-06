affc970ee686ae7bd4947cf4f2dcf2d9b7e252f7
Tiaan Louw <tiaanl@gmail.com>
2021-05-06 12:09:20 +0200

    Major cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,264 +1,300 @@
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <cstring>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 
-// These includes have to be in this order.
-#include <GL/glew.h>
-#include <windows.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "SFML/Window/Event.hpp"
-#include "SFML/Window/Window.hpp"
-#include "SFML/Graphics/Texture.hpp"
+constexpr int DISPLAY_WIDTH = 1600;
+constexpr int DISPLAY_HEIGHT = 900;
 
-enum {
-  DISPLAY_WIDTH = 800,
-  DISPLAY_HEIGHT = 600,
+void GLError() {
+#if !defined(NDEBUG)
+  auto error = glGetError();
+
+  if (error != GL_NO_ERROR) {
+    std::cerr << "OpenGL error: " << error << '\n';
+    std::exit(1);
+#if defined(WIN32)
+    __debugbreak();
+#endif
+  }
+#endif
+}
+
+const char* kVertexShader = R"(
+#version 430
+
+layout(location = 0) in vec3 in_position;
+layout(location = 1) in vec4 in_color;
+layout(location = 2) in vec2 in_tex_coord;
+
+out vec4 vertex_color;
+out vec2 vertex_tex_coord;
+
+void main() {
+  gl_Position = vec4(in_position, 1.0);
+  vertex_color = in_color;
+  vertex_tex_coord = in_position.xy;
+}
+)";
+
+const char* kFragmentShader = R"(
+#version 430
+
+in vec4 vertex_color;
+in vec2 vertex_tex_coord;
+
+out vec4 frag_color;
+
+uniform sampler2D u_texture;
+
+void main () {
+  vec4 color = texture(u_texture, vertex_tex_coord);
+  frag_color = color;
+  // frag_color = vec4(vertex_color);
+}
+)";
+
+struct Vertex {
+  glm::vec3 position;
+  glm::vec4 color;
+  glm::vec2 tex_coords;
 };
 
-const char *kVertexShader =
-    "#version 400\n"
-    "\n"
-    "layout(location = 0) in vec2 in_Position;\n"
-    //"layout(location = 1) in vec2 in_TexCoord;\n"
-    "\n"
-    //"uniform mat4 projectionMatrix;\n"
-    //"uniform mat4 modelMatrix;\n"
-    "\n"
-    //"out vec4 VertexColor;\n"
-    //"out vec2 TexCoord;\n"
-    "\n"
-    "void main() {\n"
-    //"  TexCoord = in_TexCoord;\n"
-    //"  gl_Position = \n"
-    //"      projectionMatrix * modelMatrix * \n"
-    //"      vec4(in_Position, 0.0, 1.0);\n"
-    "  gl_Position = vec4(in_Position, 0.0, 1.0);\n"
-    "}\n";
+struct Mesh {
+  GLuint vertex_array_object;
+  GLsizei vertex_count;
 
-const char *kFragmentShader =
-    "#version 400\n"
-    "\n"
-    //"in vec2 TexCoord;\n"
-    "\n"
-    //"uniform sampler2D Sampler;\n"
-    "\n"
-    "out vec4 FragmentColor;\n"
-    "\n"
-    "void main () {\n"
-    //"  FragmentColor = texture(Sampler, TexCoord);\n"
-    "  FragmentColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
-    "}\n";
+  ~Mesh() {
+    glDeleteVertexArrays(1, &vertex_array_object);
+    GLError();
+  }
+};
 
-void CheckError() {
-  int error = glGetError();
-  if (error != GL_NO_ERROR)
-    __debugbreak();
+Mesh create_mesh(Vertex* vertices, size_t count) {
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // Create the vertex buffer.
+  GLuint vertex_buffer;
+  glGenBuffers(1, &vertex_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  glBufferData(GL_ARRAY_BUFFER, GLuint(count * sizeof(Vertex)), vertices, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
+
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(float) * 3));
+
+  glEnableVertexAttribArray(2);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(float) * 7));
+
+  glBindVertexArray(0);
+
+  return {
+      vao,
+      (GLsizei)count,
+  };
 }
 
-void Log(std::string msg) {
-  OutputDebugStringA(msg.c_str());
-  OutputDebugStringA("\n");
+struct Texture {
+  GLuint texture_id;
+  int width;
+  int height;
+};
+
+Texture load_texture(std::string_view path) {
+  Texture result{};
+
+  int channels = 0;
+  auto* image = stbi_load(path.data(), &result.width, &result.height, &channels, 4);
+
+  std::cout << "Image: width(" << result.width << "), height(" << result.height << "), channels("
+            << channels << ")\n";
+
+  glGenTextures(1, &result.texture_id);
+  GLError();
+
+  glBindTexture(GL_TEXTURE_2D, result.texture_id);
+  GLError();
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, result.width, result.height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+               image);
+  GLError();
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  GLError();
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  GLError();
+
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  stbi_image_free(image);
+
+  return {};
 }
 
-GLuint CreateShader(GLenum type, std::string source) {
+GLuint CreateShader(GLenum type, const char* source) {
   GLuint id = glCreateShader(type);
-  CheckError();
+  GLError();
 
-  const GLchar *s = static_cast<const GLchar *>(source.c_str());
-  GLint length = static_cast<GLint>(source.length());
+  const GLchar* s = source;
+  auto length = static_cast<GLint>(std::strlen(source));
   glShaderSource(id, 1, &s, &length);
-  CheckError();
+  GLError();
 
   return id;
 }
 
-GLuint CreateProgram(std::string vertexShaderSource,
-                     std::string fragmentShaderSource) {
-  GLuint vertexShaderId =
-      CreateShader(GL_VERTEX_SHADER, std::move(vertexShaderSource));
-
-  GLuint fragmentShaderId =
-      CreateShader(GL_FRAGMENT_SHADER, std::move(fragmentShaderSource));
+GLuint CreateProgram(const char* vertexShaderSource, const char* fragmentShaderSource) {
+  GLuint vertexShaderId = CreateShader(GL_VERTEX_SHADER, vertexShaderSource);
+  GLuint fragmentShaderId = CreateShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
 
   // Create and compile the program.
   GLuint programId = glCreateProgram();
-  CheckError();
+  GLError();
 
   glAttachShader(programId, vertexShaderId);
-  CheckError();
+  GLError();
 
   glAttachShader(programId, fragmentShaderId);
-  CheckError();
+  GLError();
 
   glLinkProgram(programId);
-  CheckError();
+  GLError();
 
   // Print out some information about the program.
   std::string buffer(1024, 0);
   GLsizei outLength = 0;
-  glGetProgramInfoLog(programId, static_cast<GLsizei>(buffer.size()),
-                      &outLength, (GLchar *)buffer.c_str());
-  buffer[outLength] = 0;
-
-  if (!buffer.empty())
-    Log(std::move(buffer));
+  glGetProgramInfoLog(programId, static_cast<GLsizei>(buffer.size()), &outLength,
+                      (GLchar*)buffer.data());
+  buffer.resize(outLength);
+  if (!buffer.empty()) {
+    std::cerr << "Error in shader:\n" << buffer;
+  }
 
   return programId;
 }
 
-int main(int argc, char *argv[]) {
-  sf::ContextSettings settings(0, 0, 0, 4, 2, sf::ContextSettings::Default);
-  sf::Window window(sf::VideoMode(DISPLAY_WIDTH, DISPLAY_HEIGHT),
-                    "Core Graphics", sf::Style::Titlebar | sf::Style::Close,
-                    settings);
-  window.setVerticalSyncEnabled(true);
-  CheckError();
+void render_mesh(GLuint program, GLuint mode, GLuint mesh, size_t count, GLuint texture) {
+  glUseProgram(program);
+  GLError();
 
-  Log(std::move(std::string("OpenGL: ").append(
-      reinterpret_cast<const char *>(glGetString(GL_VERSION)))));
+  glBindVertexArray(mesh);
+  GLError();
 
-  glewExperimental = GL_TRUE;
+  if (texture) {
+    glActiveTexture(GL_TEXTURE0 + 0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    GLError();
+  }
+
+  auto location = glGetUniformLocation(program, "u_texture");
+  glUniform1i(location, 0);
+  GLError();
+
+  glDrawArrays(mode, 0, (GLsizei)count);
+  GLError();
+}
+
+void render_mesh(GLuint program, GLuint mode, const Mesh& mesh, Texture* texture) {
+  render_mesh(program, mode, mesh.vertex_array_object, mesh.vertex_count,
+              texture ? texture->texture_id : 0);
+}
+
+void window_size_changed(GLFWwindow* window, int width, int height) {
+  std::cout << "size changed " << width << ", " << height << '\n';
+  glViewport(0, 0, width, height);
+  GLError();
+}
+
+int main(int argc, char* argv[]) {
+  auto init_res = glfwInit();
+  if (init_res != GLFW_TRUE) {
+    return 1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window =
+      glfwCreateWindow(DISPLAY_WIDTH, DISPLAY_HEIGHT, "Core Graphics", nullptr, nullptr);
+  if (!window) {
+    return 1;
+  }
+  GLError();
+
+  glfwSetWindowSizeCallback(window, window_size_changed);
+
+  glfwMakeContextCurrent(window);
+
+  // glewExperimental = GL_TRUE;
   GLenum err = glewInit();
   if (err != GLEW_OK) {
+    std::cerr << "Could not initialize GLEW.\n";
     return 1;
   }
   glGetError();
 
-  Log(std::move(std::string("GLEW: ").append(
-      reinterpret_cast<const char *>(glewGetString(GLEW_VERSION)))));
+  std::cout << "OpenGL version: " << glGetString(GL_VERSION) << '\n';
+  std::cout << "GLEW: " << glewGetString(GLEW_VERSION) << '\n';
 
-  CheckError();
+  GLError();
+
   // Create the program we're going to use for rendering.
   GLuint programId = CreateProgram(kVertexShader, kFragmentShader);
   if (!programId) {
-    Log("Could not create program.");
+    std::cerr << "Could not create program.\n";
     return 1;
   }
 
   glUseProgram(programId);
-  CheckError();
-
-  // Load the texture.
-  sf::Texture texture;
-  std::string texturePath(__FILE__);
-  texturePath = texturePath.substr(0, texturePath.find_last_of('\\'));
-  texturePath = texturePath.substr(0, texturePath.find_last_of('\\'));
-  texturePath.append("\\resources\\block_test.png");
-  Log(texturePath);
-
-  if (!texture.loadFromFile(texturePath)) {
-    Log("Error loading texture. (" + texturePath + ")");
-    return 1;
-  }
-
-  sf::Texture::bind(&texture);
-  CheckError();
-
-#if 0
-  // Set up the matrices.
-
-  // Create our perspective projection matrix.
-  glm::mat4 projectionMatrix =
-      glm::ortho(0.f, static_cast<float>(DISPLAY_WIDTH),
-                 static_cast<float>(DISPLAY_HEIGHT), 0.f, -1.f, 1.f);
-
-  GLint projectionMatrixLocation =
-      glGetUniformLocation(programId, "projectionMatrix");
-  glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE,
-                     &projectionMatrix[0][0]);
-
-  int x = 15;
-  int y = 20;
-  int width = 256;
-  int height = 192;
-  GLfloat modelMatrix[16] = {
-      static_cast<float>(width), 0.0f,                       0.0f, 0.0f,
-      0.0f,                      static_cast<float>(height), 0.0f, 0.0f,
-      0.0f,                      0.0f,                       1.0f, 0.0f,
-      static_cast<float>(x),     static_cast<float>(y),      0.0f, 1.0f,
-  };
-  GLint modelMatrixId = glGetUniformLocation(programId, "modelMatrix");
-  glUniformMatrix4fv(modelMatrixId, 1, GL_FALSE, modelMatrix);
-#endif  // 0
+  GLError();
 
   // Set up the geometry.
+  constexpr float size = 0.5;
 
-  GLfloat points[] = {
-      0.0f, 0.0f,
-      1.0f, 0.0f,
-      1.0f, 1.0f,
-      0.0f, 1.0f,
-  };
-  GLfloat texCoords[] = {
-      0.0f, 0.0f,
-      1.0f, 0.0f,
-      1.0f, 1.0f,
-      0.0f, 1.0f,
+  Vertex vertices[] = {
+      {{0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+      {{size, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},
+      {{size, size, 0.0f}, {0.0f, 0.0f, 1.0f, 1.0f}, {1.0f, 1.0f}},
+      {{0.0f, size, 0.0f}, {1.0f, 1.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
   };
 
-  // Create the vertex array.
-  GLuint vaoId = 0;
-  glGenVertexArrays(1, &vaoId);
-  CheckError();
+  auto mesh = create_mesh(vertices, sizeof(vertices) / sizeof(Vertex));
 
-  glBindVertexArray(vaoId);
-  CheckError();
+  auto texture = load_texture("/home/tilo/code/core-graphics/resources/block_test.png");
+  glBindTexture(GL_TEXTURE_2D, texture.texture_id);
+  GLError();
 
-  GLuint vboId[2];
-  glGenBuffers(2, vboId);
-  CheckError();
-
-  // Create array buffer and bind it to attribute 0 on the vertex array.
-  glBindBuffer(GL_ARRAY_BUFFER, vboId[0]);
-  CheckError();
-  glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
-  CheckError();
-  glEnableVertexAttribArray(0);
-  CheckError();
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
-  CheckError();
-
-  // Create the texture coordinates array.
-  glBindBuffer(GL_ARRAY_BUFFER, vboId[1]);
-  CheckError();
-  glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
-  CheckError();
-  glEnableVertexAttribArray(1);
-  CheckError();
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, NULL);
-  CheckError();
-
-  glUseProgram(programId);
-  CheckError();
-  glBindVertexArray(vaoId);
-  CheckError();
-
-  while (window.isOpen()) {
-    sf::Event evt;
-    if (window.pollEvent(evt)) {
-      if (evt.type == sf::Event::Closed)
-        window.close();
-    }
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
 
     glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    CheckError();
+    GLError();
 
-    glBindVertexArray(vaoId);
-    CheckError();
+    render_mesh(programId, GL_TRIANGLE_FAN, mesh, &texture);
 
-    glDrawArrays(GL_POINTS, 0, 4);
-    CheckError();
-
-    window.display();
+    glfwSwapBuffers(window);
   }
-
-  // Delete the vertex array object.
-  glDeleteVertexArrays(1, &vaoId);
 
   // Delete the program.
   glDeleteProgram(programId);
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
   return 0;
 }
